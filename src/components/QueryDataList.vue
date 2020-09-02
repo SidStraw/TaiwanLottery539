3652349f57b35425bac4ecb083e9dfde5e6af008
Sid-Straw <j80304xz@gmail.com>
2020-09-02 21:11:05 +0800

    feat: 計算查詢頁

--- src/components/QueryDataList.vue
+++ src/components/QueryDataList.vue
@@ -8,13 +8,6 @@
     sort-by="calories"
     class="elevation-1"
   >
-    <!-- <template v-slot:top>
-      <v-toolbar flat color="white">
-        <v-toolbar-title>計算查詢結果</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-        <v-spacer></v-spacer>
-      </v-toolbar>
-    </template> -->
     <template v-slot:no-data>
       沒有符合的資料
     </template>
@@ -23,8 +16,13 @@
 
 <script>
 export default {
+  props: {
+    desserts: {
+      type: Array,
+      default: () => [],
+    },
+  },
   data: () => ({
-    dialog: false,
     headers: [
       { text: '開獎日期', value: 'date', sortable: false },
       { text: '數字1', value: 'num[0]', align: 'center', sortable: false },
@@ -33,81 +31,6 @@ export default {
       { text: '數字4', value: 'num[3]', align: 'center', sortable: false },
       { text: '數字5', value: 'num[4]', align: 'center', sortable: false },
     ],
-    desserts: [],
-    editedIndex: -1,
-    editedItem: {
-      name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
-    },
-    defaultItem: {
-      name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
-    },
   }),
-
-  computed: {
-    formTitle() {
-      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-    },
-  },
-
-  watch: {
-    dialog(val) {
-      val || this.close()
-    },
-  },
-
-  created() {
-    this.initialize()
-  },
-
-  methods: {
-    initialize() {
-      this.desserts = [
-        {
-          date: 'Frozen Yogurt',
-          n1: 6,
-          n2: 21,
-          n3: 26,
-          n4: 27,
-          n5: 34,
-        },
-      ]
-    },
-
-    editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
-    },
-
-    deleteItem(item) {
-      const index = this.desserts.indexOf(item)
-      confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
-    },
-
-    close() {
-      this.dialog = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
-
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem)
-      } else {
-        this.desserts.push(this.editedItem)
-      }
-      this.close()
-    },
-  },
 }
 </script>
--- src/views/Query.vue
+++ src/views/Query.vue
@@ -3,25 +3,25 @@
     <v-row class="rule-area text-center justify-center">
       <v-col cols="12" sm="5" lg="3" class="d-flex align-center">
         <div class="text">當某期開出</div>
-        <v-text-field dense></v-text-field>
+        <v-text-field v-model="condition.num1" dense></v-text-field>
         <div class="text">，</div>
       </v-col>
       <v-col cols="12" sm="7" lg="4" class="d-flex align-center">
         <div class="text">下</div>
-        <v-text-field dense></v-text-field>
+        <v-text-field v-model="condition.next1" dense></v-text-field>
         <div class="text">期開出</div>
-        <v-text-field dense></v-text-field>
+        <v-text-field v-model="condition.num2" dense></v-text-field>
         <div class="text">，</div>
       </v-col>
       <!-- </v-row>
     <v-row class="text-center justify-center"> -->
       <v-col cols="12" sm="6" md="5" lg="4" class="d-flex align-center">
         <div class="text">下</div>
-        <v-text-field dense></v-text-field>
+        <v-text-field v-model="condition.next2" dense></v-text-field>
         <div class="text">期開出之日期、號碼：</div>
       </v-col>
     </v-row>
-    <QueryDataList />
+    <QueryDataList :desserts="desserts" />
     <v-textarea class="mt-5" rows="3" label="備註" auto-grow></v-textarea>
   </v-container>
 </template>
@@ -32,8 +32,38 @@ import QueryDataList from '@/components/QueryDataList'
 
 export default {
   name: 'Query',
+  data() {
+    return {
+      condition: {
+        num1: '',
+        num2: '',
+        next1: '',
+        next2: '',
+      },
+    }
+  },
   components: {
     QueryDataList,
+  },
+  computed: {
+    desserts() {
+      let { num1, num2, next1, next2 } = this.condition
+      num1 = num1.replace(/^(\d)$/, '0$1')
+      num2 = num2.replace(/^(\d)$/, '0$1')
+      next1 = parseInt(next1, 10) || 0
+      next2 = parseInt(next2, 10) || 0
+      if (!num1 || !num2 || !next1 || !next2) return []
+      const { lotteryRecord } = this.$store.state
+      const result = []
+      lotteryRecord.some((item, index, array) => {
+        if (index + next1 + next2 >= array.length) return true
+        const rule2 = array[index + next2].num.some(val => val === num2)
+        const rule1 = array[index + next1 + next2].num.some(val => val === num1)
+        if (rule2 && rule1) result.push(item)
+        return result.length >= 12
+      })
+      return result
+    },
   },
 }
 </script>
